e6ee5a4f312d9888251aa62f49815f2f83194277
planepaper <eddysonkr@gmail.com>
2020-09-06 16:55:50 +0900

    Convert Java to Kotlin from backend-demo-kotlin

---
+++ src/main/java/com/example/demo/payload/request/LoginRequest.kt
@@ -0,0 +1,8 @@
+package com.example.demo.payload.request
+
+import javax.validation.constraints.NotBlank
+
+class LoginRequest {
+    var username: @NotBlank String? = null
+    var password: @NotBlank String? = null
+}
